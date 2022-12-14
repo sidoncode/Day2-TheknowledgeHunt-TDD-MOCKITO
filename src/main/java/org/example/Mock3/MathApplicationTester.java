e16f876ea896d3e382be9ddb968da7d4ee617889
sidoncode <siddharth54330@gmail.com>
2022-12-14 13:36:38 +0530

    mockito example3

--- src/main/java/org/example/Mock3/MathApplicationTester.java
+++ src/main/java/org/example/Mock3/MathApplicationTester.java
@@ -1,6 +1,7 @@
 package org.example.Mock3;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -20,23 +21,43 @@ public class MathApplicationTester {
     @Mock
     CalculatorService calculatorService;
 
+
+    @Before
+    public void Setup(){
+        mathsApplication = new MathsApplication();
+        calculatorService = mock(CalculatorService.class);
+        mathsApplication.setCalculatorService(calculatorService);
+    }
+
     @Test
+    public void testAndaddSubstract(){
+        when(calculatorService.add(20.0,10.0)).thenReturn(30.0);
+
+        when(calculatorService.substract(20.0,10.0)).thenReturn(10.0);
+
+        when(calculatorService.multiply(10.0,20.0)).thenReturn(200.0);
+        verify(calculatorService).add(20.0,10.0);
+        verify(calculatorService).substract(20.0,10.0);
+        verify(calculatorService).multiply(20.0,10.0);
+    }
+    @Test(expected = RuntimeException.class)
     public void testAdd(){
         when(calculatorService.add(10.0,20.0)).thenReturn(30.00);
 
-        Assert.assertEquals(mathsApplication.add(10.0,20.0),30.0,0);
-
         // verify the behaviour of the class
         // verify(calculatorService).add(10.0,20.0);
-
         //verify(calculatorService,times(3)).add(10.0,20.0);
-        verify(calculatorService,never()).multiply(10.0,20.0);
-
+        //verify(calculatorService,never()).multiply(10.0,20.0);
         //verify(calculatorService,atLeastOnce()).substract(20.0,10.0);
-
         //verify(calculatorService,atLeast(1)).add(10.0,20.0);
-
         //verify(calculatorService,atMost(3)).add(10.0,20.0);
+        // add the behaviour to throw exceptions //
+
+
+        doThrow(new RuntimeException("")).when(calculatorService).add(10.0,20.0);
+
+        // test the add functionality //
+        Assert.assertEquals(mathsApplication.add(10.0,20.0),30.0,0);
 
 
 
